c70206b995392fd72da77c38d5c724347f99b922
Your Name <you@example.com>
2023-05-29 16:34:20 +0300

    add all constants from refference

--- utilities/mediums.py
+++ utilities/mediums.py
@@ -1,10 +1,10 @@
 from utilities.constants import (Density, Longitudinal_velocity, Shear_velocity)
 class Medium():
-	def __init__(self, density: Density, longitudinal_velocity: Longitudinal_velocity, shear_velocity: Shear_velocity)-> float:
+	def __init__(self, density: Density, longitudinal_velocity: Longitudinal_velocity, shear_velocity: Shear_velocity):
 		self.density = density
 		self.longitudinal_velocity = longitudinal_velocity
 		self.shear_velocity = shear_velocity
-	def get_longitudinal_impedance(self):
+	def get_longitudinal_impedance(self) -> float:
 		return self.density * self.longitudinal_velocity
-	def get_shear_impedance(self):
+	def get_shear_impedance(self) -> float : 
 		return self.density*self.shear_velocity
