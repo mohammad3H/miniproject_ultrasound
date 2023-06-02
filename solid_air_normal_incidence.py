27aef936e5670110789ab46d6ee3fdccdecf334e
Your Name <you@example.com>
2023-06-02 23:24:57 +0300

    finished changes but need work on units

--- solid_air_normal_incidence.py
+++ solid_air_normal_incidence.py
@@ -6,18 +6,18 @@ class Interface():
 		self.medium1 = medium1
 		self.medium2 = medium2
 		self.propagation_mode = propagation_mode
-		
+
 	def get_normal_reflexion_ratio(self):
 		if self.propagation_mode == PropagationMode.Longitudinal:
 			r = self.medium2.get_longitudinal_impedance()/self.medium1.get_longitudinal_impedance()
-		if self.propagation_mode == PropagationMode.Longitudinal:
+		if self.propagation_mode == PropagationMode.Shear:
 			r = self.medium2.get_shear_impedance()/self.medium1.get_shear_impedance()
-		return ((r - 1) / (r + 1))^2
+		return ((r - 1) / (r + 1)) ** 2
 	
 	def get_normal_transmission_ratio(self):
 		if self.propagation_mode == PropagationMode.Longitudinal:
 			r = self.medium2.get_longitudinal_impedance()/self.medium1.get_longitudinal_impedance()
-		if self.propagation_mode == PropagationMode.Longitudinal:
+		if self.propagation_mode == PropagationMode.Shear:
 			r = self.medium2.get_shear_impedance()/self.medium1.get_shear_impedance()
-		return ((2 * r) / (r + 1))^2
+		return ((2 * r) / (r + 1)) ** 2
 
--- utilities/mediums.py
+++ utilities/mediums.py
@@ -1,10 +1,10 @@
-from utilities.constants import (Density, Longitudinal_velocity, Shear_velocity)
+from utilities.constants import (Density, LongitudinalVelocity, ShearVelocity)
 class Medium():
-	def __init__(self, density: Density, longitudinal_velocity: Longitudinal_velocity, shear_velocity: Shear_velocity):
+	def __init__(self, density: Density, longitudinal_velocity: LongitudinalVelocity, shear_velocity: ShearVelocity):
 		self.density = density
 		self.longitudinal_velocity = longitudinal_velocity
 		self.shear_velocity = shear_velocity
 	def get_longitudinal_impedance(self) -> float:
 		return self.density.value * self.longitudinal_velocity.value
 	def get_shear_impedance(self) -> float : 
-		return self.density.value*self.shear_velocity.value
+		return self.density.value * self.shear_velocity.value
