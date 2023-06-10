8ef23a1dbc813ff071624010ceb90a784308a3de
Your Name <you@example.com>
2023-06-10 21:26:52 +0300

    first changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # miniproject_ultrasound
+
+# part 1:
+this part consist of plotting the reflection and transmission coefficient of available materials and compare it to experimental data.
+
+# part 2:
+in part 2 we will do it for a continuous impedance difference.
--- utilities/mediums.py
+++ utilities/mediums.py
@@ -1,10 +1,10 @@
 from utilities.constants import (Density, LongitudinalVelocity, ShearVelocity)
 class Medium():
-	def __init__(self, density: Density, longitudinal_velocity: LongitudinalVelocity, shear_velocity: ShearVelocity):
+	def __init__(self, density: float, longitudinal_velocity: float, shear_velocity: float):
 		self.density = density
 		self.longitudinal_velocity = longitudinal_velocity
 		self.shear_velocity = shear_velocity
 	def get_longitudinal_impedance(self) -> float:
-		return self.density.value * self.longitudinal_velocity.value
+		return self.density * self.longitudinal_velocity
 	def get_shear_impedance(self) -> float : 
-		return self.density.value * self.shear_velocity.value
+		return self.density*self.shear_velocity
